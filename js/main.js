3bab1efd6f625c8a34ec74c9046fea7e7afef77d
Joe Liang <56658669+1000mileworld@users.noreply.github.com>
2020-06-01 18:46:31 -0400

    courses done and half through testimonials

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel();
 });
 
-$('.owl-carousel').owlCarousel({
+$('.services .owl-carousel').owlCarousel({
     loop:true, 
     margin:10,
     responsiveClass:true,
@@ -15,6 +15,23 @@ $('.owl-carousel').owlCarousel({
         },
         1000:{
             items:4,
+        }
+    }
+})
+
+$('.testimonials .owl-carousel').owlCarousel({
+    loop:true, 
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:1,
         }
     }
 })
